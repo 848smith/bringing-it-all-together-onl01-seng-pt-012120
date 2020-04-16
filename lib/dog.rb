2ac1b3d66c750a7230487897d8859633a49c38d4
Andrew Smith <848smith@gmail.com>
2020-04-16 02:45:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,10 @@ class Dog
     dog
   end
   
-  def self find_by_id
-    
-  end
+  def self find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ?
+      SQL
+    DB[:conn].execute(sql, id)
+  end.first
 end
