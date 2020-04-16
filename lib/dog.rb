c1b4d8c2745e0b07a1d4e980b1130f8645c55afe
Andrew Smith <848smith@gmail.com>
2020-04-16 02:40:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,8 +35,8 @@ class Dog
     self
   end
   
-  def self.create(name:, breed:)
-    dog = Dog.new(name:, breed:)
+  def self.create(hash)
+    dog = Dog.new(name: , breed:)
     dog.save
     dog
   end
