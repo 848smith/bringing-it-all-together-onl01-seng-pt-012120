63a748ba8d84d151c8b352d0aceecfd1af26ebd3
Andrew Smith <848smith@gmail.com>
2020-04-16 03:07:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,6 +67,8 @@ class Dog
   end
   
   def self.find_by_name(name)
-    
+    DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
